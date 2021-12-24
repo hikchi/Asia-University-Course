8ea371fbf76e352c583e5ba278ae569aefa29a15
KC <kc@KCdeMacBook-Pro.local>
2021-12-24 10:32:49 +0800

    demo

--- src/js/app.js
+++ src/js/app.js
@@ -33,7 +33,7 @@ App = {
       // 取得編譯過後的相關資料
       // const AsiaUniversityBankArtifact = data;
       // 取得合約地址
-      const AsiaUniversityBankAddress = "0xcBa34b7Ed4001b636EC5A4EFD45917D6e363484A";
+      const AsiaUniversityBankAddress = "<改成自己部署的地址>";
 
       // [Web3]] 初始化合約
       // 需要合約的ABI, 跟地址(address)
@@ -95,6 +95,17 @@ App = {
       const withdrawAmountInWei = web3.utils.toWei(`${withdrawAmount}`, 'ether');
 
       // [Web3] 
+      // 需要轉換成AU幣的單位，也就是10**18，因此要傳的是withdrawAmountInWei
+      App.contracts.AsiaUniversityBank.methods.withdraw(withdrawAmountInWei).send({
+        // 從目前連接的帳戶發送交易
+        from: account,
+      }).then(function (receipt) {
+        alert("提款成功！")
+        // 再次確認AU幣的餘額
+        return App.getBalances()
+      }).catch(function (error) {
+        console.log(error.message)
+      })
 
     });
   },
@@ -116,6 +127,21 @@ App = {
 
       const depositAmountInWei = web3.utils.toWei(`${depositAmount}`, 'ether');
       // [Web3] 
+      // 取得合約實例
+      // 發送合約deposit函式
+      App.contracts.AsiaUniversityBank.methods.deposit().send({
+        // 從目前連接的帳戶發送交易
+        from: account,
+        // value: 這筆交易我要送多少ETH進去，合約會以msg.value呈現
+        // [Important] 單位要用wei，不可以用ether
+        value: depositAmountInWei,
+      }).then(function (receipt) {
+        alert("存款成功！")
+        // 再次確認AU幣的餘額
+        return App.getBalances()
+      }).catch(function (error) {
+        console.log(error.message)
+      })
 
     });
   },
@@ -138,6 +164,17 @@ App = {
 
       const mintedAmountInWei = web3.utils.toWei(`${mintedAmount}`, 'ether');
       // [Web3] 
+      // 需要轉換成AU幣的單位，也就是10**18，因此要傳的是mintedAmountInWei
+      App.contracts.AsiaUniversityBank.methods.mint(mintedAddress, mintedAmountInWei).send({
+        // 從目前連接的帳戶發送交易
+        from: account,
+      }).then(function (receipt) {
+        alert("鑄幣成功！")
+        // 再次確認AU幣的餘額
+        return App.getBalances()
+      }).catch(function (error) {
+        console.log(error.message)
+      })
 
     });
   },
@@ -186,12 +223,11 @@ App = {
 
     });
   },
-
+  // 取得用戶的AU幣的餘額
   getBalances: function () {
     console.log('Getting balances...');
 
     let AsiaUniversityBankInstance;
-
     web3.eth.getAccounts(function (error, accounts) {
       if (error) {
         console.log(error);
@@ -202,6 +238,20 @@ App = {
 
       // [Web3] 取得你的餘額
       // 需要先在Metamask新增自己在Ganache的帳戶
+      // 怎麼做呢？
+      // 1. 取得合約實例
+      // 2. 呼叫balanceOf函式
+      // App.contracts.AsiaUniversityBank
+      App.contracts.AsiaUniversityBank.methods.balanceOf(account).call()
+        .then(function (result) {
+          // 一單位的AU幣實際上是10的18次方，顯示的時候需要把單位除回去
+          const balance = result / 10 ** 18
+          console.log(balance)
+          // 把id為AUBalance的HTML元件抓出來，並且改變他的文字(text)
+          $('#AUBalance').text(balance)
+        }).catch(function (error) {
+          console.log(error.message)
+        })
 
     });
   }
